9a7ce92b2b7fbb569ddf4a44f881ab3ba8d36c8d
Dale Syth <dalesyth@gmail.com>
2023-06-24 15:07:23 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan("dev"));
+
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
 
 const PORT = 1337;
 
